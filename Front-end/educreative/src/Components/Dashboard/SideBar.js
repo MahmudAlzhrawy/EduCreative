32bb4e4639e5d44a6aa012daf0e624cca10651b3
nadwaso2000 <nadwaso2000@gmail.com>
2024-10-24 09:34:07 +0300

    update

--- Front-end/educreative/src/Components/Dashboard/SideBar.js
+++ Front-end/educreative/src/Components/Dashboard/SideBar.js
@@ -16,15 +16,16 @@ export default function SideBar()
     return(
         <div style={{ position: WindowStatus.windowSize2 < '900' ? 'fixed' : 'sticky', 
                       left: WindowStatus.windowSize2 < '900' && MenuButton.Open ? '-100%' : '0',
-                      width: WindowStatus.windowSize2 < '900' ? 'fit-content' : '20%'
+                      width: WindowStatus.windowSize2 < '900' ? 'fit-content' : '24%'
                     }} 
-                      className="w-[20%] bg-[#F5FAEE] flex justify-center items-center h-screen">
-            <div className="w-full h-[480px] flex justify-start py-6 md:pl-5 pl-1  items-start bg-[#F5FAEE] flex-col">
-                <NavLink className="text-[#3C4532] mt-[25px] font-[Poppins] font-normal text-[20px] gap-2 flex justify-center items-center hover:bg-gray-300 rounded-[14px] p-2"><BiSolidHomeAlt2 className="w-[39.38px] h-[41.52px]" />{WindowStatus.windowSize2 > '900' && <p>Home</p>} </NavLink>
-                <NavLink className="text-[#3C4532]  mt-[25px] font-[Poppins] font-normal text-[20px] gap-2 flex justify-center items-center hover:bg-gray-300 rounded-[14px] p-2"><IoNotifications className="w-[39.38px] h-[41.52px]"/>{ WindowStatus.windowSize2 > '900' && <p>Notifications</p>}</NavLink>
-                <NavLink className="text-[#3C4532]  mt-[25px] font-[Poppins] font-normal text-[20px] gap-2 flex justify-center items-center hover:bg-gray-300 rounded-[14px] p-2"><FaUserCircle className="w-[39.38px] h-[41.52px]" />{WindowStatus.windowSize2 > '900' && <p>Profile</p>}</NavLink>
-                <NavLink to='/dashboard/materials' className="text-[#3C4532]  mt-[25px] font-[Poppins] font-normal text-[20px] flex justify-center items-center gap-2 hover:bg-gray-300 rounded-[14px] p-2"><TfiViewGrid className="w-[37.38px] h-[39.52px] text-[#3C4532] fill-current"/>{ WindowStatus.windowSize2 > '900' && <p>Materials</p>}</NavLink>
-                <NavLink className="text-[#3C4532]  mt-[25px] font-[Poppins] font-normal text-[20px] flex justify-center items-center gap-2 hover:bg-gray-300 rounded-[14px] p-2"><PiNotepadFill className="w-[39.38px] h-[41.52px]" /> { WindowStatus.windowSize2 > '900' && <p>Notes</p>}</NavLink>
+             className="w-[24%] bg-[#F5FAEE] flex justify-center items-center h-screen"
+            >
+            <div className="w-full h-[480px] flex justify-start py-6 md:pl-4 pl-0  items-start bg-[#F5FAEE] flex-col">
+                <NavLink className="text-[#3C4532] mt-[25px] font-[Poppins] font-normal text-[20px] gap-2 flex justify-center items-center hover:bg-gray-300 rounded-[14px] p-2"><BiSolidHomeAlt2 className="w-[30.34px] h-[33.35px] sm:w-[39.38px] sm:h-[41.52px]" />{WindowStatus.windowSize2 > '900' && <p>Home</p>} </NavLink>
+                <NavLink to='/dashboard/notifications' className="text-[#3C4532]  mt-[25px] font-[Poppins] font-normal text-[20px] gap-2 flex justify-center items-center hover:bg-gray-300 rounded-[14px] p-2"><IoNotifications className="w-[30.34px] h-[33.35px] sm:w-[39.38px] sm:h-[41.52px]"/>{ WindowStatus.windowSize2 > '900' && <p>Notifications</p>}</NavLink>
+                <NavLink to='/dashboard/profile' className="text-[#3C4532]  mt-[25px] font-[Poppins] font-normal text-[20px] gap-2 flex justify-center items-center hover:bg-gray-300 rounded-[14px] p-2"><FaUserCircle className="w-[30.34px] h-[33.35px] sm:w-[39.38px] sm:h-[41.52px]" />{WindowStatus.windowSize2 > '900' && <p>Profile</p>}</NavLink>
+                <NavLink to='/dashboard/materials' className="text-[#3C4532]  mt-[25px] font-[Poppins] font-normal text-[20px] flex justify-center items-center gap-2 hover:bg-gray-300 rounded-[14px] p-2"><TfiViewGrid className="w-[30.34px] h-[33.35px] sm:w-[37.38px] sm:h-[39.52px] text-[#3C4532] fill-current"/>{ WindowStatus.windowSize2 > '900' && <p>Materials</p>}</NavLink>
+                <NavLink to='/dashboard/notes' className="text-[#3C4532] mt-[25px] font-[Poppins] font-normal text-[20px] flex justify-center items-center gap-2 hover:bg-gray-300 rounded-[14px] p-2"><PiNotepadFill className="w-[30.34px] h-[33.35px] sm:w-[39.38px] sm:h-[41.52px]" /> { WindowStatus.windowSize2 > '900' && <p>Notes</p>}</NavLink>
             </div>
         </div>
     )
